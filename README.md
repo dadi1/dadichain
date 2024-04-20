04ef9cdbed929519796d054c881533312b3d1581
dadi1 <narayan.shimanoe@gmail.com>
2024-04-20 17:29:01 -0300

    class blockchain created

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # dadichain
-Simple blockchain roject done based on [this](https://hackernoon.com/learn-blockchains-by-building-one-117428612f46)site tutorial.
+Simple blockchain roject done based on [this](https://hackernoon.com/learn-blockchains-by-building-one-117428612f46) site tutorial.
 
---
+++ blockchain.py
@@ -0,0 +1,22 @@
+class Blockchain (object):
+    def __init__(self):
+        self.chain = []
+        self.current_transactions = []
+
+    def new_blocks(self):
+        #creates new blocks
+        pass
+
+    def new_transactions(self):
+        # adds a new transaction to the list of transactions
+        pass
+
+    @staticmethod
+    def hash(block):
+        # hashes a block
+        pass
+
+    @staticmethod
+    def hash(block):
+        # returns the last block in the chain
+        pass
